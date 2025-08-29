33a87ace35b1a729212d6083c3597758137b7faa
unknown <rizqikurni29@gmail.com>
2025-08-29 15:01:27 +0700

    Tambahan Animasi

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <StarBackground />
-  <Header/>
+  <Header />
+  <Asteroid :pos="{ x: -0.5, y: 2, z: 1 }" :scale="2" />
   <!-- <DotLottieVue ref="astronautRef" :src="spaceLottie" autoplay loop class="astronaut" /> -->
   <!-- <Astronot /> -->
   <Home />
   <Skill />
-  <Website/>
-     <Project />
+  <Website />
+  <Project />
 </template>
-
 
 <script setup>
 import { ref, onMounted, nextTick } from "vue";
@@ -18,11 +18,9 @@ import Project from './views/Project.vue';
 import StarBackground from './views/Background.vue';
 import Header from "./views/Header.vue";
 import Website from "./views/Website.vue";
+import Asteroid from "./views/Asteroid.vue";
 // import Astronot from "./views/Astronot.vue";
-
 </script>
-
-
 
 <style scoped>
 </style>
